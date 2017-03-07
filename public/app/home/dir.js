e145f0114be77a9ee8de5118ad6ff330e7c2850b
Joe Blank <joe.blank@live.com>
2017-03-07 09:38:33 -0700

    added text and photos to services section. added some styling.

--- public/app/home/dir.js
+++ public/app/home/dir.js
@@ -3,23 +3,51 @@ angular.module('prosults')
   return {
     restrict: 'A',
     link: (scope, elem, attrs) => {
-      // console.log(elem);
-      // $('.pro').on('mouseenter', () => {
-      //   $('.sults').text('FESSIONAL')
-      // })
-      // $('.pro').on('mouseleave', () => {
-      //   $('.sults').text('SULTS')
-      // })
+
       $(window).scroll(() => {
         let scrollTracker = $(window).scrollTop();
         if(scrollTracker > 50) {
           // $('.prosults, .about, .contact, .services').fadeOut();
           $('.nav-container').fadeOut(750);
-          
+
         } else {
           $('.nav-container').fadeIn(750);
         }
       })
+
+      $('.strategy-image').on('mouseenter', function() {
+        $(this).css({
+          "background-size": "195%"
+        })
+      })
+      $('.strategy-image').on('mouseleave', function() {
+        $(this).css({
+          "background-size": "187%"
+        })
+      })
+
+      $('.analytics-image').on('mouseenter', function() {
+        $(this).css({
+          "background-size": "160%"
+        })
+      })
+      $('.analytics-image').on('mouseleave', function() {
+        $(this).css({
+          "background-size": "152%"
+        })
+      })
+
+      $('.sustainability-image').on('mouseenter', function() {
+        $(this).css({
+          "background-size": "171%"
+        })
+      })
+      $('.sustainability-image').on('mouseleave', function() {
+        $(this).css({
+          "background-size": "163%"
+        })
+      })
+
     }
 
   }
